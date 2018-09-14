00204cd3c18f0f3a6bd216f95bb0e7487bfc8e10
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-09-14 15:00:46 -0700

    add "use strict" to all JavaScript files

--- routes/logout.js
+++ routes/logout.js
@@ -1,11 +1,8 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
-var users = require('../json_objects/users.json');
-
-var username = undefined;
-var password = undefined;
-var token = undefined;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
