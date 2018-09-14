32c26e66088c9a9a970a297345fa1a887073bab1
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-09-14 13:54:44 -0700

    clean up and bug fix

--- routes/register.js
+++ routes/register.js
@@ -4,9 +4,11 @@ var router = express.Router();
 var fs = require('fs');
 var users = require('../json_objects/users.json');
 
+var registerCtr = require('../clientjs/registerCtr');
+
 router.get('/', function (req, res, next) {
     if (req.session.token) {
-        res.render('profile', {username: req.session.username, token: req.session.token, message: "You already registered!"});
+        res.render('profile', { username: req.session.username, token: req.session.token, message: "You already registered!" });
     } else {
         res.render('register');
     }
@@ -17,6 +19,18 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
+
+    // should not rely on front end to check the data
+    // double check the data in back end
+    var result = registerCtr.validate(req.body);
+    if (!result.pass) {
+        var error = {
+            status: 'Register Error',
+            stack: result.errorMessage
+        };
+
+        return res.render('error', { error: error });
+    }
 
     users[username] = password;
     var json = JSON.stringify(users, null, 4);
