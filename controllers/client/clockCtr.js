fe44575204947c4b9542bf22d13392f43a416abe
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-09-16 18:53:20 -0700

    clean up code

--- controllers/client/clockCtr.js
+++ controllers/client/clockCtr.js
@@ -14,38 +14,38 @@ clockCtr.drawClock = function drawClock() {
 
 clockCtr.drawFace = function drawFace(ctx, radius) {
     var grad;
-    clockCtr.ctx.beginPath();
-    clockCtr.ctx.arc(0, 0, clockCtr.radius, 0, 2 * Math.PI);
-    clockCtr.ctx.fillStyle = 'white';
-    clockCtr.ctx.fill();
-    grad = clockCtr.ctx.createRadialGradient(0, 0, clockCtr.radius * 0.95, 0, 0, clockCtr.radius * 1.05);
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
     grad.addColorStop(0, '#333');
     grad.addColorStop(0.5, 'white');
     grad.addColorStop(1, '#333');
-    clockCtr.ctx.strokeStyle = grad;
-    clockCtr.ctx.lineWidth = clockCtr.radius * 0.1;
-    clockCtr.ctx.stroke();
-    clockCtr.ctx.beginPath();
-    clockCtr.ctx.arc(0, 0, clockCtr.radius * 0.1, 0, 2 * Math.PI);
-    clockCtr.ctx.fillStyle = '#333';
-    clockCtr.ctx.fill();
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
 };
 
 clockCtr.drawNumbers = function drawNumbers(ctx, radius) {
     var ang;
     var num;
-    clockCtr.ctx.font = clockCtr.radius * 0.15 + "px arial";
-    clockCtr.ctx.textBaseline = "middle";
-    clockCtr.ctx.textAlign = "center";
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
     for (num = 1; num < 13; num++) {
         ang = num * Math.PI / 6;
-        clockCtr.ctx.rotate(ang);
-        clockCtr.ctx.translate(0, -clockCtr.radius * 0.85);
-        clockCtr.ctx.rotate(-ang);
-        clockCtr.ctx.fillText(num.toString(), 0, 0);
-        clockCtr.ctx.rotate(ang);
-        clockCtr.ctx.translate(0, clockCtr.radius * 0.85);
-        clockCtr.ctx.rotate(-ang);
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
     }
 };
 
@@ -59,24 +59,24 @@ clockCtr.drawTime = function drawTime(ctx, radius) {
     hour = (hour * Math.PI / 6) +
         (minute * Math.PI / (6 * 60)) +
         (second * Math.PI / (360 * 60));
-        clockCtr.drawHand(clockCtr.ctx, hour, clockCtr.radius * 0.5, clockCtr.radius * 0.07);
+        clockCtr.drawHand(ctx, hour, radius * 0.5, radius * 0.07);
     //minute
     minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-    clockCtr.drawHand(clockCtr.ctx, minute, clockCtr.radius * 0.8, clockCtr.radius * 0.07);
+    clockCtr.drawHand(ctx, minute, radius * 0.8, radius * 0.07);
     // second
     second = (second * Math.PI / 30);
-    clockCtr.drawHand(clockCtr.ctx, second, clockCtr.radius * 0.9, clockCtr.radius * 0.02);
+    clockCtr.drawHand(ctx, second, radius * 0.9, radius * 0.02);
 };
 
 clockCtr.drawHand = function drawHand(ctx, pos, length, width) {
-    clockCtr.ctx.beginPath();
-    clockCtr.ctx.lineWidth = width;
-    clockCtr.ctx.lineCap = "round";
-    clockCtr.ctx.moveTo(0, 0);
-    clockCtr.ctx.rotate(pos);
-    clockCtr.ctx.lineTo(0, -length);
-    clockCtr.ctx.stroke();
-    clockCtr.ctx.rotate(-pos);
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 };
 
 setInterval(clockCtr.drawClock, 1000);
