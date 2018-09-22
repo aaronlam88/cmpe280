229ab1c05a97f51cc6ced41234d344c7eaecd8f5
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-09-22 12:49:54 -0700

    more clean up and update

---
+++ controllers/client/googleMapCtr.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// name your object same as your file name
+// wrap all your functions in a object for closure and avoid namespace collision
+// include the javascript file in the correct pug file that need this javascript
+// think of this as your class (in ES6 we will have class)
+var googleMap = (function () {
+    // ==== class variables ====
+
+    // ==== functions ====
+    function mapInit() {
+        var mapProp = {
+            center: new google.maps.LatLng(37.3351916,-121.8832655),
+            zoom: 12,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    }
+    // expose functions or variables in the return
+    // ==> make functions or variables in the return public
+    return {
+        init: mapInit
+    }
+})();
+
+// ==== onload --> call the following functions ====
+
+
--- controllers/client/planeCtr.js
+++ controllers/client/planeCtr.js
@@ -6,15 +6,13 @@
 // think of this as your class (in ES6 we will have class)
 var plantCtr = (function () {
     // ==== class variables ====
-    var CANVAS_W = 1000;
-    var CANVAS_H = 200;
-    var IMAGE_W = 100;
-    var IMAGE_H = 100;
+    var CANVAS_W = 800;
+    var CANVAS_H = 100;
 
     var x = 0;
     var dx = 10;
 
-    var con = document.getElementById("canvas").getContext("2d");
+    var con = document.getElementById("plane").getContext("2d");
 
     var image = new Image();
     image.src = "https://img.clipartxtras.com/7a99f4b3b6998c86b57a292aac76d4fa_airport-airports-icon-036430-icons-etc-white-airplane-clipart-no-background_512-512.png";
@@ -22,16 +20,13 @@ var plantCtr = (function () {
     // ==== functions ====
 
     var draw = function () {
-        con.strokeStyle = "white";
-        con.fillStyle = "white";
-        con.fillRect(0, 0, CANVAS_W, CANVAS_H);
-        con.strokeRect(0, 0, CANVAS_W, CANVAS_H);
+        con.clearRect(0, 0, CANVAS_W, CANVAS_H);
 
         if (x > 800) x = 0;
 
         con.save();
 
-        con.drawImage(image, x, 0, IMAGE_W, IMAGE_H);
+        con.drawImage(image, x, 0, 100, 100);
         x += dx
         con.restore();
     };
