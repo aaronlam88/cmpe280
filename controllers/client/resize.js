69e84836f8edb7b7116994db763b3917d1f848e3
Moon <moonqiyue@yahoo.com>
2018-09-29 23:14:44 -0700

    resize in progress

---
+++ controllers/client/resize.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#resize").resizable();
+    $("div").addClass("ui-widget")
+            .addClass("ui-widget-content")
+            .addClass("ui-corner-all");
+    $(":header").addClass("ui-widget-header")
+                .addClass("ui-corner-all");
+});
