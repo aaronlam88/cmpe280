5fd81da9f559134ada4ef2320dfc534392bb661f
Moon <moonqiyue@yahoo.com>
2018-10-01 23:29:42 -0700

    Final Version of Drag&Drop

--- controllers/client/dragDrop.js
+++ controllers/client/dragDrop.js
@@ -1,26 +1,5 @@
-$(function() {
-    $( "#dragMe li" ).draggable({
-        helper: 'clone',
-        revert: 'invalid'
-    });
-    $( "#target" ).droppable({
-        accept: 'li[data-value="country"]',
-        drop: function (event, ui) {
-          $('#target').append(ui.draggable);
-      }
-    });
-    //$("#target").bind("drop", highlightTarget);
-    $("#target").bind("dropout", resetTarget);
-  });
-
-// function highlightTarget(event, ui){
-//     $("#target").addClass("ui-state-highlight")
-//                 .html("You got me")
-//                 //.append(ui.draggable.text());
-// } 
-  
-
-function resetTarget(event, ui){
-    $("#target").removeClass("ui-state-highlight")
-                .html("Drop on me");
-}
+$( function() {
+    $( "#sortable1, #sortable2" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  } );
