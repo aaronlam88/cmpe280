e49784211e32a7555447237fb25a181c398de477
FanWu <fwulunch@gmail.com>
2018-10-02 17:17:54 -0700

    update search page with jquery-ui tab/button

---
+++ controllers/client/buttons.js
@@ -0,0 +1,11 @@
+$( function() {
+    $("#b1").click( function() {
+      var googleMap = new GoogleMap(35.676687,139.6716328, 13);
+    } );
+    $("#b2").click( function() {
+      var googleMap = new GoogleMap(37.7884388,-122.4391559, 14);
+    } );
+    $("#b3").click( function() {
+      var googleMap = new GoogleMap(48.8570576,2.3049119, 15);
+    } );
+  } );
--- controllers/client/googleMapCtr.js
+++ controllers/client/googleMapCtr.js
@@ -4,24 +4,44 @@
 // wrap all your functions in a object for closure and avoid namespace collision
 // include the javascript file in the correct pug file that need this javascript
 // think of this as your class (in ES6 we will have class)
-var googleMap = (function () {
-    // ==== class variables ====
-
+var GoogleMap = function (x, y, z) {
     // ==== functions ====
-    function mapInit() {
-        var mapProp = {
-            center: new google.maps.LatLng(37.3351916,-121.8832655),
-            zoom: 12,
-        };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    }
+      var location = {lat: x, lng: y};
+
+      var map = new google.maps.Map(document.getElementById("googleMap"), {
+        center : location,
+        zoom : z
+      });
+
+      var infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.textSearch({
+        location: location,
+        radius: 500,
+        query: 'hotel'
+      }, callback);
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
     // expose functions or variables in the return
     // ==> make functions or variables in the return public
-    return {
-        init: mapInit
-    }
-})();
+};
 
 // ==== onload --> call the following functions ====
-
-
