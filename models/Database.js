46ce494dfb33dfecb75a14d890ef7a12a652030a
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-10-27 23:47:26 -0700

    mongodb connection done

--- models/Database.js
+++ models/Database.js
@@ -4,61 +4,167 @@ const MongoClient = require('mongodb').MongoClient;
 
 // Default connection url
 var _url = 'mongodb://18.207.178.32:27017';
-// Default database Name
-var _database = 'cmpe280';
+// Default database name
+var _databaseName = 'cmpe280';
+// Database
+var _database = undefined;
 
-/**
- * Constructor for Database
- * If you want to connect to a database other than the default, give a database in the param
- * If you want to connect to a different mongodb host other than the default, give the url in the param
- * @param {*} database database name that you want to connect to, default: 'aaronDB'
- * @param {*} url url used to connect to mongodb, default: 'mongodb://18.207.178.32:27017'
- */
-function Database(database, url) {
-    // Database Name
-    _database = database || _database;
-    // Connection url
-    _url = url || _url;
-}
+class Database {
 
-Database.prototype.createCollection = function (collection) {
-    if (!collection) {
-        return { 'status': 'ERROR', 'message': 'Missing collection!' };
-    }
-    try {
-        MongoClient.connect(_url, { useNewUrlParser: true }, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db(_database);
-            dbo.createCollection(collection, function (err, res) {
-                if (err) throw err;
-                console.log(`${collection} created!`);
-                db.close();
-            });
+    /**
+     * If you want to connect to a database other than the default, give a databaseName in the param
+     * If you want to connect to a different mongodb host other than the default, give the url in the param
+     * @param {string} databaseName database name, default: 'cmpe280'
+     * @param {string} url mongodb url, default: 'mongodb://18.207.178.32:27017'
+     */
+    constructor(databaseName, url) {
+        // Database Name
+        _databaseName = databaseName || _databaseName;
+        // Connection url
+        _url = url || _url;
+        // connect to database
+        MongoClient.connect(_url, { useNewUrlParser: true }, function (error, database) {
+            if (error) {
+                console.log(error);
+                throw error;
+            }
+            _database = database.db(_databaseName);
         });
-        return { 'status': 'OK', 'message': `${collection} created!` };
-    } catch (error) {
-        return { 'status': 'ERROR', 'message': error };
     }
-}
 
-Database.prototype.insert = function (collection, data) {
-    if (!collection) {
-        return { 'status': 'ERROR', 'message': 'Missing collection!' };
-    }
-    try {
-        MongoClient.connect(_url, { useNewUrlParser: true }, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db(_database);
-            dbo.collection(collection).insertMany(data, (err, res) => {
-                if (err) throw err;
-                var count = res.insertedCount;
-                console.log(`${res.insertedCount} object(s) inserted into collection ${collection}`);
-                db.close();
+    /**
+     * create new collection if collection is not existed in current database
+     * @param {object} respond Express.respond object, use to respond to a request
+     * @param {string} collection name of the collection
+     */
+    createCollection(respond, collection) {
+        if (!collection) {
+            respond.json({ 'status': 'ERROR', 'message': 'Missing collection!' });
+        }
+        try {
+            _database.createCollection(collection, function (error, result) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+                console.log(`collection [${collection}] is created!`);
+                // you can do respond.render(view, data) here
+                respond.json({
+                    status: 'OK',
+                    message: `collection [${collection}] is created!`,
+                    data: []
+                });
             });
-        });
-        return { 'status': 'OK', 'message': `inserted` };
-    } catch (error) {
-        return { 'status': 'ERROR', 'message': error };
+        }
+        catch (error) {
+            respond.json({ 'status': 'ERROR', 'message': error });
+        }
+    }
+
+    /**
+     * insert data into collection
+     * @param {object} respond Express.respond object, use to respond to a request
+     * @param {string} collection name of the collection
+     * @param {Array} data an array of json object 
+     */
+    insert(respond, collection, data) {
+        if (!collection) {
+            respond.json({ 'status': 'ERROR', 'message': 'Missing collection!' });
+        }
+        try {
+            _database.collection(collection).insertMany(data, function (error, result) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+                console.log(`${result.insertedCount} object(s) inserted into collection [${collection}]`);
+
+                // you can do respond.render(view, data) here
+                respond.json({
+                    status: 'OK',
+                    message: `${result.insertedCount} object(s) inserted into collection [${collection}]`,
+                    data: result
+                });
+            });
+        }
+        catch (error) {
+            respond.json({ 'status': 'ERROR', 'message': error });
+        }
+    }
+
+    find(respond, collection, data) {
+        if (!collection) {
+            respond.json({ 'status': 'ERROR', 'message': 'Missing collection!' });
+        }
+
+        try {
+            _database.collection(collection).find(data).toArray(function (error, result) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+                console.log(result);
+                // you can do respond.render(view, data) here
+                respond.json({
+                    status: 'OK',
+                    message: `find ${JSON.stringify(data)} in collection [${collection}]`,
+                    data: result
+                });
+            });
+        }
+        catch (error) {
+            respond.json({ 'status': 'ERROR', 'message': error });
+        }
+    }
+
+    findAndRemove(respond, collection, data) {
+        if (!collection) {
+            respond.json({ 'status': 'ERROR', 'message': 'Missing collection!' });
+        }
+
+        try {
+            _database.collection(collection).findAndRemove(data, function (error, result) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+                console.log(result);
+                // you can do respond.render(view, data) here
+                respond.json({
+                    status: 'OK',
+                    message: `findAndRemove ${JSON.stringify(data)} in collection [${collection}]`,
+                    data: result
+                });
+            });
+        }
+        catch (error) {
+            respond.json({ 'status': 'ERROR', 'message': error });
+        }
+    }
+
+    update(respond, collection, query, data) {
+        if (!collection) {
+            respond.json({ 'status': 'ERROR', 'message': 'Missing collection!' });
+        }
+
+        try {
+            _database.collection(collection).update(query, { $set: data }, function (error, result) {
+                if (error) {
+                    console.log(error);
+                    throw error;
+                }
+                console.log(result);
+                // you can do respond.render(view, data) here
+                respond.json({
+                    status: 'OK',
+                    message: `update ${JSON.stringify(query)} in collection [${collection}]`,
+                    data: result
+                });
+            });
+        }
+        catch (error) {
+            respond.json({ 'status': 'ERROR', 'message': error });
+        }
     }
 }
 
--- routes/mongodb.js
+++ routes/mongodb.js
@@ -6,31 +6,157 @@ var router = express.Router();
 var Database = require('../models/Database');
 
 var help = {
-    '[GET] /createCollection?collection=...' : 'create a new collection in the database',
-    '[GET] /insert?collection=...&data=[{...}]' : 'insert data into the collection [not recommended]',
-    '[POST] /insert' : 'insert object {"collection": ..., data: [{...}] } in the post body into the collection [recommended]'
+    '[GET] /createCollection?collection=...': 'create a new collection in the database',
+    '[GET] /insert?collection=...&data=[{...}]': 'insert data into the collection [not recommended]',
+    '[POST] /insert': 'insert object {"collection": ..., data: [{...}] } in the post body into the collection [recommended]'
 };
+var collectionFieldError = {
+    status: 'collection field error',
+    stack: 'Require: collection field must be a string'
+};
+var insertDataFieldError = {
+    status: 'data field error',
+    stack: 'Require: data field must be an Array of JSON objects'
+};
+var findDataFieldError = {
+    status: 'data field error',
+    stack: 'Require: data field must be of JSON type object and NOT Array'
+};
+var updateQueryFieldError = {
+    status: 'query field error',
+    stack: 'Require: new field must be of JSON type object and NOT Array'
+};
+var updateDataFieldError = {
+    status: 'data field error',
+    stack: 'Require: data field must be of JSON type object and NOT Array'
+};
+
 // default route
 router.get('/', function (req, res, next) {
     res.json(help);
 });
 
+// create a connection to database
 var database = new Database();
+
+// create collection
 router.get('/createCollection', function (req, res, next) {
     var collection = req.query.collection;
-    res.json(database.createCollection(collection));
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    database.createCollection(res, collection);
+});
+router.post('/createCollection', function (req, res, next) {
+    var collection = req.body.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    database.createCollection(res, collection);
 });
 
+// insert data into collection
 router.get('/insert', function (req, res, next) {
     var collection = req.query.collection;
-    var data = JSON.parse(req.query.data);
-    res.json(database.insert(collection, data));
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var data = JSON.parse(req.query.data || "[]");
+    if (!Array.isArray(data) || data.length === 0) {
+        return res.render('error', { error: insertDataFieldError });
+    }
+    database.insert(res, collection, data);
 });
-
 router.post('/insert', function (req, res, next) {
     var collection = req.body.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
     var data = req.body.data;
-    res.json(database.insert(collection, data));
+    if (!Array.isArray(data) || data.length === 0) {
+        return res.render('error', { error: insertDataFieldError });
+    }
+    database.insert(res, collection, data);
+});
+
+// update data into collection
+router.get('/update', function (req, res, next) {
+    var collection = req.query.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var query = JSON.parse(req.query.query || "{}");
+    if (typeof query !== 'object' || query.length === 0) {
+        return res.render('error', { error: updateQueryFieldError });
+    }
+    var data = JSON.parse(req.query.data || "{}");
+    if (typeof data !== 'object' || data.length === 0) {
+        return res.render('error', { error: updateDataFieldError });
+    }
+    database.update(res, collection, query, data);
+});
+router.post('/update', function (req, res, next) {
+    var collection = req.body.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var query = req.body.query;
+    if (typeof query !== 'object' || query.length === 0) {
+        return res.render('error', { error: updateQueryFieldError });
+    }
+    var data = req.body.data;
+    if (typeof data !== 'object' || data.length === 0) {
+        return res.render('error', { error: updateDataFieldError });
+    }
+    database.update(res, collection, query, data);
+});
+
+// find data into collection
+router.get('/find', function (req, res, next) {
+    var collection = req.query.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var data = JSON.parse(req.query.data || "{}");
+    if (typeof data !== 'object' || Array.isArray(data)) {
+        return res.render('error', { error: findDataFieldError });
+    }
+    database.find(res, collection, data);
+});
+router.post('/find', function (req, res, next) {
+    var collection = req.body.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var data = req.body.data;
+    if (typeof data !== 'object' || Array.isArray(data)) {
+        return res.render('error', { error: findDataFieldError });
+    }
+    database.find(res, collection, data);
+});
+
+// find and remove data into collection
+router.get('/findAndRemove', function (req, res, next) {
+    var collection = req.query.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var data = JSON.parse(req.query.data || "{}");
+    if (typeof data !== 'object' || Array.isArray(data) || data.length === 0) {
+        return res.render('error', { error: findDataFieldError });
+    }
+    database.findAndRemove(res, collection, data);
+});
+router.post('/findAndRemove', function (req, res, next) {
+    var collection = req.body.collection;
+    if (typeof collection !== 'string') {
+        return res.render('error', { error: collectionFieldError });
+    }
+    var data = req.body.data;
+    if (typeof data !== 'object' || Array.isArray(data) || data.length === 0) {
+        return res.render('error', { error: findDataFieldError });
+    }
+    database.findAndRemove(res, collection, data);
 });
 
 module.exports = router;
