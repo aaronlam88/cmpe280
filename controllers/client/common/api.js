10069e349538e9f77d46361f65fee318e80a7ebd
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-10-29 12:08:29 -0700

    code clean up and add template

--- controllers/client/common/api.js
+++ controllers/client/common/api.js
@@ -1,34 +1,43 @@
+"use strict";
+
+// name your object same as your file name
+// wrap all your functions in a object for closure and avoid namespace collision
+// include the javascript file in the correct pug file that need this javascript
+// think of this as your class (in ES6 we will have class)
+
 /**
  * an api to get data
  */
-const api = {
+var api = (function () {
+    // ==== class variables ====
+
+    // ==== functions ====
     /**
-     * get data from backend server using jQuery get or local cache with localStorage
-     * will call $.get, will do window.dispatchEvent(url)
-     * to get the data, use window.addEventListener(url, (event) => (event.data))
-     * @param {string} url 
+     * get data from backend server using jQuery get
+     * will do window.dispatchEvent(eventID)
+     * to get the data, use window.addEventListener(url, (event) => {handle return data})
+     * @param {String} url url to get data from
+     * @param {String} eventID the eventID that should be emitted when data return
      */
-    jQueryGet(url, eventID) {
-        $.get(
-            url,
-        ).done((data) => {
-            console.debug('success')
-        }).fail(() => {
-            console.error('fail');
-        }).always((data) => {
-            let event = new Event(eventID);
-            event.data = data;
-            window.dispatchEvent(event);
-        });
-    },
+    function jQueryGet(url, eventID) {
+        $.ajax({
+            url: url,
+            success: function (result) {
+                let event = new Event(eventID);
+                event.data = result;
+                window.dispatchEvent(event);
+            }
+        })
+    }
 
     /**
-     * get data from backend server using jQuery post or local cache with localStorage
-     * will call $.get, will do window.dispatchEvent(url)
-     * to get the data, use window.addEventListener(url, (event) => (event.data))
-     * @param {string} url 
+     * get data from backend server using jQuery post
+     * will do window.dispatchEvent(eventID)
+     * to get the data, use window.addEventListener(url, (event) => (handle return data))
+     * @param {String} url url to get data from
+     * @param {String} eventID the eventID that should be emitted when data return
      */
-    jQueryPost(url, data, eventID) {
+    function jQueryPost(url, data, eventID) {
         $.ajax({
                 type: "POST",
                 url: url,
@@ -42,4 +51,13 @@ const api = {
                 }
             })
     }
-}
+
+    // expose functions or variables in the return
+    // ==> make functions or variables in the return public
+    return {
+        jQueryGet: jQueryGet,
+        jQueryPost: jQueryPost
+    }
+})();
+
+// ==== onload --> call the following functions ====
