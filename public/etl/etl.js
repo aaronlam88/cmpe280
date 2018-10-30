a1d11e5c0c5780e5c360acd60a61db8b0ef26ae7
FanWu <fwulunch@gmail.com>
2018-10-29 20:37:57 -0700

    etl script

--- public/etl/etl.js
+++ public/etl/etl.js
@@ -5,11 +5,9 @@ var etl = require('etl');
 var db = monk('mongodb://18.207.178.32:27017/cmpe280');
 var collection = db.get('etl');
 
-var parse = require('csv-parse')
-
 fs.createReadStream('toProcess.csv')
   .pipe(etl.csv())
   .pipe(etl.collect(1000))
-  .pipe(etl.collection.insert(collection))
+  .pipe(etl.mongo.insert(collection))
   .promise()
   .then( () => console.log('done'), e => console.log('error',e));
