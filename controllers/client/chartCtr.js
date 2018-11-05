f131e35d8d31e657f75951a740399f25c8fc3e82
FanWu <fwulunch@gmail.com>
2018-11-05 14:44:52 -0800

    4-charts template, need to change the data

--- controllers/client/chartCtr.js
+++ controllers/client/chartCtr.js
@@ -1,53 +1,79 @@
-var ctx = document.getElementById("myChart");
+var ctx1 = document.getElementById("myChart1");
+var ctx2 = document.getElementById("myChart2");
+var ctx3 = document.getElementById("myChart3");
+var ctx4 = document.getElementById("myChart4");
 var currentURL = 'http://' + window.location.hostname + ':' + window.location.port;
 
 window.addEventListener('dataIsReady', (event) => {
   var d = event.data.data[0].data;
-  // var temp = (d) => {
-  //
-  // }
-
-
+  var city = [];
+  var count = [];
+  for (var i=0; i < d.length-1; i++) {
+    city.push(d[i]['city']);
+    count.push(d[i]['count']);
+  }
   var options =
   {
-    type: 'bubble',
+    type: 'horizontalBar',
     data: {
+        labels: city,
         datasets: [{
-            label: 'Scatter',
-            data: d,
+            label: "Top 10 cities",
+            data: count,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(151, 239, 144, 0.2)',
+                'rgba(116, 68, 88, 0.2)',
+                'rgba(238, 181, 86, 0.2)',
+                'rgba(70, 219, 17, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(151, 239, 144, 1)',
+              'rgba(116, 68, 88, 1)',
+              'rgba(238, 181, 86, 1)',
+              'rgba(70, 219, 17, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
-        scales: {
-          xAxes: [{
-              type: 'linear',
-              position: 'bottom'
-          }]
+      scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
         },
-        responsive: 'false'
+        // scales: {
+        //   xAxes: [{
+        //       type: 'linear',
+        //       position: 'bottom'
+        //   }]
+        // },
+        responsive: 'true'
     }
   }
-  var myChart = new Chart(ctx, options);
+  var myChart1 = new Chart(ctx1, options);
+  var myChart2 = new Chart(ctx2, options);
+  var myChart3 = new Chart(ctx3, options);
+  var myChart4 = new Chart(ctx4, options);
+
 });
 
 var find = function() {
-    var data = {"collection": "chart_data", "data":{}};
+    var data = {"collection": "chart_data", "data":{"id":2}};
+
     api.jQueryPost(currentURL + '/mongodb/find', data, 'dataIsReady');
 }();
--- routes/chart.js
+++ routes/chart.js
@@ -7,34 +7,45 @@ var collection = database.get('hotel_review');
 
 // default route
 router.get('/', function (req, res, next) {
-    // var allData = []
-    // collection.find({},{latitude:1, longitude:1}, () => {}).each(docs => {
-    //   allData.push({x: parseFloat(docs.latitude), y:parseFloat(docs.longitude)});
+//     var allData = []
+//     collection.find({},{latitude:1, longitude:1}, () => {}).each(docs => {
+//       allData.push({x: parseFloat(docs.latitude), y:parseFloat(docs.longitude)});
+//     }).then(() => {
+//       console.log('done!');
+//     }).catch(err => {
+//       console.log(err);
+//     })
+//
+
+    // function sortByKey(array, key) {
+    //     return array.sort(function(a, b) {
+    //         var x = a[key]; var y = b[key];
+    //         return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    //     });
+    // }
+    // var allData = {};
+    // var arr = [];
+    // collection.find({}, () => {}).each(docs => {
+    //   var city = docs.city + ", " + docs.province + ", " + docs.postalCode;
+    //   if(!(city in allData)) {
+    //     allData[city] = 1;
+    //   }
+    //   else {
+    //     allData[city] += 1;
+    //   }
     // }).then(() => {
+    //   for (var key in allData) {
+    //     let temp = {"city": key, "count": allData[key]};
+    //     arr.push(temp);
+    //   }
+    //   var t = sortByKey(arr, 'count').slice(0,11);
+    //   database.get('chart_data').insert({"data":t , "id":2}).then(()=>{}).catch(err=>{
+    //     console.log(err);
+    //   });
     //   console.log('done!');
     // }).catch(err => {
     //   console.log(err);
-    // })
-    //
-    // var options = {
-    //   type: 'scatter',
-    //   data: {
-    //     datasets: [{
-    //       label: 'Scatter',
-    //       data: allData
-    //     }]
-    //   },
-    //   options: {
-    //     scales: {
-    //       xAxes: [{
-    //         type: 'linear',
-    //         position: 'bottom'
-    //       }]
-    //     },
-    //     responsive: false
-    //   }
-    // };
-
+    // });
     res.render('chart');
 });
 
