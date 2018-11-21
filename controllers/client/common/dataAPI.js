e5955eda36fd6ea00f47cde83d1dd1be9fdb64ac
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-11-20 18:05:47 -0800

    update init

--- controllers/client/common/dataAPI.js
+++ controllers/client/common/dataAPI.js
@@ -4,12 +4,42 @@
 var dataAPI = (function () {
     // ==== class variables ====
     var currentURL = 'http://' + window.location.hostname + ':' + window.location.port;
+    var rawData = undefined;
+
+    function init() {
+        api.jQueryPost(currentURL + '/mongodb/find', {collection: 'hotel_review', data: {}}, 'dataIsReady');
+
+        window.addEventListener('dataIsReady', (event) => dataIsReady(event));
+    }
+
+    function dataIsReady(data) {
+        if(data.status === 'ERROR') {
+            console.log(data.message);
+        }
+        rawData = data.data;
+    }
 
     /**
      * 
      */
-    function constructor() {
-        
+    function drawUSMap(triggeredByEvent) {
+        if(!rawData && !triggeredByEvent) {
+            window.addEventListener('dataIsReady', () => drawUSMap(true));
+            return;
+        }
+        if(triggeredByEvent) {
+            window.removeEventListener('dataIsReady', drawUSMap());
+        }
+
+        console.log('drawUSMap');
     }
-    
+    // expose functions or variables in the return
+    // ==> make functions or variables in the return public
+    return {
+        init: init,
+        drawUSMap: drawUSMap
+    }
 })();
+
+dataAPI.init();
+dataAPI.drawUSMap();
