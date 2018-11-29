4a3ad21bb76c0981e98ac304704b8d801de30896
Moon <moonqiyue@yahoo.com>
2018-11-28 17:09:25 -0800

    adding tableau

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ app.use('/mongodb', require('./routes/mongodb'));
 app.use('/dashboard', require('./routes/dashboard'));
 
 app.use('/intro2', require('./routes/intro'));
+app.use('/contact', require('./routes/contact'));
+app.use('/overview', require('./routes/overview'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
